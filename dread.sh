8c45d76b4c75d90b65e7d68ede93e00831443281
Nbot2142 <92255438+Nbot2142@users.noreply.github.com>
2023-05-20 13:12:00 +0200

    Update dread.sh

--- dread.sh
+++ dread.sh
@@ -1,117 +1,77 @@
 #!/bin/bash
 
-# Ustawienie kolorów
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-WHITE='\033[1;37m'
-NC='\033[0m' # Resetowanie koloru
+# Kolory
+WHITE="\033[1;37m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+NC="\033[0m" # Normalny kolor
 
-# Funkcja skanująca sieć i wyświetlająca adresy IP i nazwy urządzeń
+# Funkcja skanująca sieć i wyświetlająca adresy IP urządzeń wraz z nazwami
 function scannw() {
     echo -e "${WHITE}[${GREEN}Dread${WHITE}] Scanning the network...${NC}"
-    
-    # Przeskanowanie sieci i zapisanie wyników do pliku tymczasowego
-    nmap -sn $network_address/24 | grep "Nmap scan report" > devices.txt
-    
-    # Wyświetlenie wyników skanowania
-    echo -e "${WHITE}[${GREEN}Dread${WHITE}] Devices found:${NC}"
-    while read -r line; do
-        ip=$(echo $line | awk '{print $NF}')
-        hostname=$(host $ip | awk '{print $NF}')
-        echo -e "${WHITE}[${GREEN}Dread${WHITE}] IP: ${GREEN}$ip${WHITE}, Hostname: ${GREEN}$hostname${NC}"
-    done < devices.txt
-    
-    # Usunięcie pliku tymczasowego
-    rm devices.txt
+    # Wprowadź odpowiednią komendę skanującą sieć, np. nmap
+    # Tutaj dodaj kod skanujący sieć i wyświetlający adresy IP urządzeń wraz z nazwami
 }
 
-# Funkcja nawiązująca połączenie z urządzeniem przez adres IP
+# Funkcja łącząca z urządzeniem o podanym adresie IP
 function cnctip() {
-    ip=$1
+    local ip=$1
     echo -e "${WHITE}[${GREEN}Dread${WHITE}] Connecting to ${GREEN}$ip${NC}"
-    
-    # Sprawdzenie, czy jest połączenie z urządzeniem o podanym adresie IP
-    if ping -c 1 $ip &> /dev/null; then
-        # Nawiązanie połączenia SSH z urządzeniem o podanym adresie IP
-        ssh $ip
-    else
-        echo -e "${WHITE}[${RED}Dread${WHITE}] Unable to connect to ${RED}$ip${NC}"
-    fi
+    # Tutaj dodaj kod łączący się z urządzeniem o podanym adresie IP
 }
 
-# Komenda odłączająca się od urządzenia
+# Funkcja odłączająca się od aktualnie połączonego urządzenia
 function uncnctip() {
-    echo -e "${WHITE}[${RED}Dread${WHITE}] Disconnecting from the device${NC}"
-    # Dodaj tutaj kod do odłączania się od urządzenia
+    echo -e "${WHITE}[${GREEN}Dread${WHITE}] Disconnecting from the connected device${NC}"
+    # Tutaj dodaj kod odłączający się od aktualnie połączonego urządzenia
 }
 
-# Komenda wysyłająca testową wiadomość
+# Funkcja wyświetlająca informacje o urządzeniu o podanym adresie IP
+function dvcinfo() {
+    local ip=$1
+    echo -e "${WHITE}[${GREEN}Dread${WHITE}] Getting device information for ${GREEN}$ip${NC}"
+    # Tutaj dodaj kod pobierający i wyświetlający informacje o urządzeniu o podanym adresie IP
+}
+
+# Funkcja wysyłająca testową wiadomość do urządzenia o podanym adresie IP
 function tsmsg() {
-    ip=$1
+    local ip=$1
     echo -e "${WHITE}[${GREEN}Dread${WHITE}] Sending test message to ${GREEN}$ip${NC}"
-    
-    # Sprawdzenie, jaki jest system na podstawie adresu IP
-    response=$(ssh $ip uname -s)
-    if [[ $response == *"Linux"* ]]; then
-        # Jeśli system to Linux, wykonaj odpowiednie działania
-        echo -e "${WHITE}[${GREEN}Dread${WHITE}] Linux system detected${NC}"
-        
-        # Wykonanie polecenia na zdalnym systemie Linux (np. wysłanie messageboxa)
-        ssh $ip 'zenity --info --text "This is a test message"'
-        
-        # Wyświetlenie komunikatu o wykonanej akcji
-        echo -e "${WHITE}[${GREEN}Dread${WHITE}] Test message sent to ${GREEN}$ip${NC}"
-    elif [[ $response == *"Windows"* ]]; then
-        # Jeśli system to Windows, wykonaj odpowiednie działania
-        echo -e "${WHITE}[${GREEN}Dread${WHITE}] Windows system detected${NC}"
-        
-        # Wykonanie polecenia na zdalnym systemie Windows (np. wysłanie messageboxa)
-        ssh $ip 'cmd /c echo MsgBox "This is a test message" > test.vbs && cscript //B test.vbs'
-        
-        # Wyświetlenie komunikatu o wykonanej akcji
-        echo -e "${WHITE}[${GREEN}Dread${WHITE}] Test message sent to ${GREEN}$ip${NC}"
-    else
-        echo -e "${WHITE}[${RED}Dread${WHITE}] Unknown system detected${NC}"
-    fi
+    # Tutaj dodaj kod wysyłający testową wiadomość do urządzenia o podanym adresie IP
 }
 
 # Wyświetlanie napisu w odpowiednich kolorach
 echo -e "${WHITE}[${GREEN}Dread${WHITE}] Dread is active${NC}"
 echo -e "${WHITE}[${GREEN}Dread${WHITE}] Version: 1.0${NC}"
-echo -e "${WHITE}[${GREEN}Dread${WHITE}] Type 'help' to display available commands${NC}"
+echo -e "${WHITE}[${GREEN}Dread${WHITE}] Type 'drdhelp' to display available commands${NC}"
 
 # Obsługa komend
 case $1 in
-    help)
+    drdhelp)
         echo -e "${WHITE}[${GREEN}Dread${WHITE}] Available commands:${NC}"
-        echo -e "${WHITE}  help - Displays available commands${NC}"
+        echo -e "${WHITE}  drdhelp - Displays available commands${NC}"
         echo -e "${WHITE}  scannw - Scans the network and displays the IP addresses of devices${NC}"
         echo -e "${WHITE}  cnctip <IP> - Connects to the device with the specified IP address${NC}"
         echo -e "${WHITE}  uncnctip - Disconnects from the connected device${NC}"
-        echo -e "${WHITE}  dvcinfo <IP> - Displays information about the specified device${NC}"
-        echo -e "${WHITE}  tsmsg <IP> - Sends a test message to the specified device${NC}"
+        echo -e "${WHITE}  dvcinfo <IP> - Displays information about the device with the specified IP address${NC}"
+        echo -e "${WHITE}  tsmsg <IP> - Sends a test message to the device with the specified IP address${NC}"
         ;;
     scannw)
-        # Wywołanie funkcji skanującej sieć
         scannw
         ;;
     cnctip)
-        # Wywołanie funkcji nawiązującej połączenie z podanym adresem IP
-        cnctip "$2"
+        cnctip $2
         ;;
     uncnctip)
-        # Wywołanie funkcji odłączającej się od urządzenia
         uncnctip
         ;;
     dvcinfo)
-        # Wywołanie funkcji wyświetlającej informacje o urządzeniu o podanym adresie IP
-        dvcinfo "$2"
+        dvcinfo $2
         ;;
     tsmsg)
-        # Wywołanie funkcji wysyłającej testową wiadomość
-        tsmsg "$2"
+        tsmsg $2
         ;;
     *)
-        echo -e "${WHITE}[${RED}Dread${WHITE}] Unknown command${NC}"
+        echo -e "${WHITE}[${GREEN}Dread${WHITE}] Unknown command${RED}! Connect to a device or use 'drdhelp' for available commands.${NC}"
         ;;
 esac
